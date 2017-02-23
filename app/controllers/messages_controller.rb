3ea07fae06af00d4ad7cd65bfaf75b25f59134b7
Ken <votantho93@gmail.com>
2017-02-23 23:45:45 +0700

    new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,22 @@ class MessagesController < ApplicationController
   def index
     @messages = current_user.lastest_received_messages(10)
   end
+
+
+  def new 
+    @message = Message.new
+    @friend_lists = Relationship.where(user_id: current_user.id, status: "accepted").or(Relationship.where(friend_id: current_user.id, status: "accepted"))
+  end
+
+  def create
+    Message.create sender: current_user, recipient: User.find_by(id: message_params[:recipient]), body: message_params[:body]
+    flash[:success] = "Sent success"
+    redirect_to new_message_path
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :recipient)
+  end
+
 end
